1a969659c60c11ef5a437f9418e5ce124396bdc5
Maciej Siekierski <maciejsiekier@gmail.com>
2017-10-05 22:12:58 +0200

    nunit tests project

---
+++ PanJanek.SokobanSolver.Tests/PositionTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using PanJanek.SokobanSolver.Sokoban;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PanJanek.SokobanSolver.Tests
+{
+    [TestFixture]
+    public class PositionTests
+    {
+        [Test]
+        public void SokobanPosition_GetUniqueId()
+        {
+            SokobanPosition pos = SokobanPosition.LoadFromFile("C:\\Projects\\git\\SokobanSolver\\levels\\sokoban1.txt");
+            var a = pos.GetUniqueId();
+        }
+    }
+}
